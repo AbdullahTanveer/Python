dc9893cf2aecf6547e6fd3d26bb64e6aa4093ed8
AbdullahTanveer <abdullahtanveer56@outlook.com>
2021-06-27 13:52:26 +0500

    Ayesha

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,8 @@ print("Hello world!!!")
 print(" a + b is " 12 + 10)
 
 """ Hamza Khalid """
-print(" 195047 - Hamza Khalid");
+print(" 195047 - Hamza Khalid")
+
+
+""" Ayesha Noor """
+print(" 195030 - Ayesha Noor")
